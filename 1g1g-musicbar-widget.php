675b25db9e183064bf85970e0fe3d8fcb86ce410
Jeff  Sole <Jeff  Sole@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-05 01:50:29 +0000

    The first version of the widget
    git-svn-id: https://plugins.svn.wordpress.org/1g1g-music-bar/trunk@213275 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 1g1g-musicbar-widget.php
@@ -0,0 +1,180 @@
+<?php
+/*  Copyright 2010  cynic  (email : 1g1g.founder@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*
+Plugin Name:  1g1g music bar widget
+Plugin URI:   http://www.1g1g.com/
+Description:  A widget of 1g1g music bar.
+Version:      0.1
+Author:       cynic
+Author URI:   http://www.1g1g.com/	
+*/
+
+	function yige_musicbar_widget($args, $widget_args = 1) {
+		
+		extract( $args, EXTR_SKIP );
+		if ( is_numeric($widget_args) )
+			$widget_args = array( 'number' => $widget_args );
+		$widget_args = wp_parse_args( $widget_args, array( 'number' => -1 ) );
+		extract( $widget_args, EXTR_SKIP );
+	
+		$options = get_option('yige_musicbar_widget');
+		if ( !isset($options[$number]) ) 
+			return;
+
+		$title = $options[$number]['title'];
+		$bgcolor = $options[$number]['bgcolor'];
+		$textcolor = $options[$number]['textcolor'];
+		$framecolor = $options[$number]['framecolor'];
+			
+		echo $before_widget; 
+		if ( ! defined( 'WP_CONTENT_URL' ) )
+      		define( 'WP_CONTENT_URL', get_option( 'siteurl' ) . '/wp-content' );
+		if ( ! defined( 'WP_CONTENT_DIR' ) )
+		    define( 'WP_CONTENT_DIR', ABSPATH . 'wp-content' );
+		if ( ! defined( 'WP_PLUGIN_URL' ) )
+		    define( 'WP_PLUGIN_URL', WP_CONTENT_URL. '/plugins' );
+		if ( ! defined( 'WP_PLUGIN_DIR' ) )
+      		define( 'WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins' );
+      	$mbpath = WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)); ?>
+		<table id="controller_1g1g" cellspacing="4px" cellpadding="0" border="0" style="table-layout:fixed;width:100%;height:24px;cursor:pointer;background-color:<?php echo $bgcolor; ?>;border:solid 1px <?php echo $framecolor; ?>;"> 
+			<tr>
+				<td id="playPauseBtn_1g1g" style="width:16px;height:16px;background-image:url('<?php echo $mbpath; ?>/1g1gasset/play-pause-next.gif');cursor:pointer;background-position:0px 0px;overflow:hidden;">
+				</td>
+				<td id="nextBtn_1g1g" style="width:16px;height:16px;background-image:url('<?php echo $mbpath; ?>/1g1gasset/play-pause-next.gif');background-position:0px -64px;cursor:pointer;overflow:hidden;">
+				</td>
+				<td valign="middle" id="displayText_1g1g" style="font-size:12px; line-height:12px; color: <?php echo $textcolor; ?>;white-space:nowrap;overflow:hidden;">
+					<?php echo $title; ?>
+				</td>
+			</tr>
+		</table>
+		<script type="text/javascript" src="<?php echo $mbpath.'/1g1gasset/controller.js'; ?>"></script>
+								
+	<?php echo $after_widget;
+	
+	}
+	
+	
+	function yige_musicbar_widget_control($widget_args) {
+	
+		global $wp_registered_widgets;
+		static $updated = false;
+	
+		if ( is_numeric($widget_args) )
+			$widget_args = array( 'number' => $widget_args );			
+		$widget_args = wp_parse_args( $widget_args, array( 'number' => -1 ) );
+		extract( $widget_args, EXTR_SKIP );
+	
+		$options = get_option('yige_musicbar_widget');
+		
+		if ( !is_array($options) )	
+			$options = array();
+	
+		if ( !$updated && !empty($_POST['sidebar']) ) {
+		
+			$sidebar = (string) $_POST['sidebar'];	
+			$sidebars_widgets = wp_get_sidebars_widgets();
+			
+			if ( isset($sidebars_widgets[$sidebar]) )
+				$this_sidebar =& $sidebars_widgets[$sidebar];
+			else
+				$this_sidebar = array();
+	
+			foreach ( (array) $this_sidebar as $_widget_id ) {
+				if ( 'yige_musicbar_widget' == $wp_registered_widgets[$_widget_id]['callback'] && isset($wp_registered_widgets[$_widget_id]['params'][0]['number']) ) {
+					$widget_number = $wp_registered_widgets[$_widget_id]['params'][0]['number'];
+					if ( !in_array( "yige_musicbar_widget-$widget_number", $_POST['widget-id'] ) ) 
+						unset($options[$widget_number]);
+				}
+			}
+	
+			foreach ( (array) $_POST['yige_musicbar_widget'] as $widget_number => $yige_musicbar_widget ) {
+				$title = strip_tags(stripslashes($yige_musicbar_widget['title_value']));
+				$bgcolor = $yige_musicbar_widget['bgcolor_value'];
+				$framecolor = $yige_musicbar_widget['framecolor_value'];
+				$textcolor = $yige_musicbar_widget['textcolor_value'];
+				$options[$widget_number] = compact( 'title', 'textcolor', 'bgcolor', 'framecolor' );
+			}
+	
+			update_option('yige_musicbar_widget', $options);
+			$updated = true;
+		}
+	
+		if ( -1 == $number ) {
+			$title = '开始听歌';
+			$textcolor = '#ffffff';
+			$bgcolor = '#5a82b5';
+			$framecolor = '#444444';
+			$number = '%i%';
+			
+		} else { 
+			$title = attribute_escape($options[$number]['title']);
+			$textcolor = $options[$number]['textcolor'];
+			$bgcolor = $options[$number]['bgcolor'];
+			$framecolor = $options[$number]['framecolor'];
+		}
+		
+	?>
+	<table cellspacing="10px" cellpadding="0" border="0" >
+	<tr>
+		<td align="right" >默认的文字</td>
+		<td ><input id="title_value_<?php echo $number; ?>" name="yige_musicbar_widget[<?php echo $number; ?>][title_value]" type="text" value="<?=$title?>" /></td>
+	</tr>
+	<tr>
+		<td align="right" >文字颜色</td>
+		<td ><input id="textcolor_value_<?php echo $number; ?>" name="yige_musicbar_widget[<?php echo $number; ?>][textcolor_value]" type="text" value="<?=$textcolor?>" /></td>
+	</tr>
+	<tr>
+		<td align="right" >背景颜色</td>
+		<td ><input id="bgcolor_value_<?php echo $number; ?>" name="yige_musicbar_widget[<?php echo $number; ?>][bgcolor_value]" type="text" value="<?=$bgcolor?>" /></td>
+	</tr>
+	<tr>
+		<td align="right" >边框颜色</td>
+		<td ><input id="framecolor_value_<?php echo $number; ?>" name="yige_musicbar_widget[<?php echo $number; ?>][framecolor_value]" type="text" value="<?=$framecolor?>" /></td>
+	</tr>
+	</table>
+    <input type="hidden" name="yige_musicbar_widget[<?php echo $number; ?>][submit]" value="1" />
+    
+	<?php
+	}
+	
+	
+	function yige_musicbar_widget_register() {
+		if ( !$options = get_option('yige_musicbar_widget') )
+			$options = array();
+		$widget_ops = array('classname' => 'yige_musicbar_widget', 'description' => __('Yige musicbar widget form'));
+		$control_ops = array('width' => 400, 'height' => 350, 'id_base' => 'yige_musicbar_widget');
+		$name = __('Yige Musicbar Widget');
+	
+		$id = false;
+		
+		foreach ( (array) array_keys($options) as $o ) {
+				
+			$id = "yige_musicbar_widget-$o";
+			wp_register_sidebar_widget($id, $name, 'yige_musicbar_widget', $widget_ops, array( 'number' => $o ));
+			wp_register_widget_control($id, $name, 'yige_musicbar_widget_control', $control_ops, array( 'number' => $o ));
+		}
+		
+		if ( !$id ) {
+			wp_register_sidebar_widget( 'yige_musicbar_widget-1', $name, 'yige_musicbar_widget', $widget_ops, array( 'number' => -1 ) );
+			wp_register_widget_control( 'yige_musicbar_widget-1', $name, 'yige_musicbar_widget_control', $control_ops, array( 'number' => -1 ) );
+		}
+	}
+
+add_action('init', yige_musicbar_widget_register, 1);
+
+?>
---
+++ readme.txt
@@ -0,0 +1,43 @@
+=== 1g1g Music Bar Widget ===
+Contributors: cynic
+Donate Link: http://www.1g1g.com
+Tags: 1g1g, music, plugin, widget
+Requires at least: 2.5
+Tested up to: 2.9.1
+Stable Tag: /trunk
+
+A simple music bar is used to control a 1g1g player, show the lyric of the playing song.
+
+== Description ==
+
+The widget can be installed in the sidebar of a blog powered by Wordpress. While the blog is loaded, the widget can open or control a 1g1g player to play music and show the lyric of the song.
+
+== Installation ==
+
+* Install directly from WordPress (just go to *Plugins* -> *Add New*) and activate.
+
+Or
+
+* Manually install to your WP plugins directory
+	1. Unzip `1g1g-musicbar.zip` in the `/wp-content/plugins/` directory.
+	2. Activate the plugin through the *Plugins* menu in WordPress.
+	3. Select a favorite color of the widget in the widgets admin page.
+
+== Changelog ==
+
+= 0.1.0 =
+* Basic music bar implemented.
+
+== Frequently Asked Questions ==
+
+= Is the 1g1g web site opened firstly? =
+No, the music bar will detect 1g1g web automatically. If the web hasn't been opened, the music bar will open a new web page of the 1g1g site, when the start button clicked. 
+
+== Upgrade Notice ==
+
+= 0.1.0 =
+* First version released.
+
+== Screenshots ==
+
+1. The style of the music bar is showed in http://public.1g1g.com/musicbar/intro.html
